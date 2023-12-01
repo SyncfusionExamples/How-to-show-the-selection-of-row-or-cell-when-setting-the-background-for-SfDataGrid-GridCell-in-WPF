e92d7579a85dfe25cce2c8b1c71eb1bc71418b1d
vijayarasan <vijayarasan.sivanandham@syncfusiom.com>
2023-12-01 12:49:28 +0530

    Added the content and sample

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# How-to-show-the-selection-of-row-or-cell-when-setting-the-background-for-SfDataGrid-GridCell-in-WPF
-How to show the selection of row or cell when setting the background for SfDataGrid GridCell in WPF
+# How to show the selection of row or cell when setting the background for SfDataGrid GridCell in WPF
+
+## Problem
+
+[Row](https://help.syncfusion.com/cr/wpf/Syncfusion.UI.Xaml.Grid.GridSelectionUnit.html)/[Cell](https://help.syncfusion.com/cr/wpf/Syncfusion.UI.Xaml.Grid.GridSelectionUnit.html) selection border is behind the [GridCell](https://help.syncfusion.com/cr/wpf/Syncfusion.UI.Xaml.Grid.GridCell.html) content. So when you apply the background for `GridCell`, the selection is not displayed in the UI. The following code example shows you how to set the `GridCell` style.
+ 
+ ```XML 
+ 
+<!--Need to add this code snippet in Resources Define the style with Key-->
+<Style x:Key="ApplyCellStyle" TargetType="Syncfusion:GridCell">
+        <Setter Property="Background">
+               <Setter.Value>
+                    <SolidColorBrush Color="Blue" />
+                </Setter.Value>
+         </Setter>          
+</Style>       
+
+<Syncfusion:SfDataGrid x:Name="datagrid" 
+                       ColumnSizer="Star" 
+                       SelectionUnit="Row"  
+                       AllowSelectionOnPointerPressed="True"  
+                       RowSelectionBrush="Red"  
+                       CellStyle="{StaticResource ApplyCellStyle}"  
+                       ItemsSource="{Binding GDCSource}"  
+                       SelectionMode="Single" >
+</Syncfusion:SfDataGrid>
+
+ ```
+
+In the following screenshot, the selection is set in the 5th row, but it is not displayed in View with the selection border in the Cell Content presenter. 
+ 
+ ![RowSelection.png](RowSelection.png)
+
+## Solution
+
+You can overcome this problem by reducing the **Opacity** for the brush that you set to the `GridCell` background. The following code example shows you how to set the **Opacity** to `GridCell`.
+ 
+ ```XML
+ 
+<!--Need to add this code snippet in Resources Define the style with Key-->
+<Style x:Key="ApplyCellStyle" TargetType="Syncfusion:GridCell">
+        <Setter Property="Background">
+               <Setter.Value>
+                    <SolidColorBrush Color="Blue" Opacity="0.5"/>
+                </Setter.Value>
+         </Setter>          
+</Style>       
+<Syncfusion:SfDataGrid x:Name="datagrid" 
+                       ColumnSizer="Star" 
+                       SelectionUnit="Row"  
+                       AllowSelectionOnPointerPressed="True"  
+                       RowSelectionBrush="Red"  
+                       CellStyle="{StaticResource ApplyCellStyle}"  
+                       ItemsSource="{Binding GDCSource}"  
+                       SelectionMode="Single" >
+</Syncfusion:SfDataGrid>
+
+ ```
+
+The following screenshot shows you the selection of row after applying the background for `GridCell`. 
+ 
+ ![SelectionofRowAfterApplyingBackGround.png](SelectionofRowAfterApplyingBackGround.png)
+
+Take a moment to peruse the [WPF DataGrid - Conditional Styling](https://help.syncfusion.com/wpf/datagrid/conditional-styling) documentation, where you can find about conditional styling with code examples.
---
+++ SfDataGridDemo/ViewModel/ViewModel.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace SfDataGridDemo
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private EmployeeDetails emp = new EmployeeDetails();
+
+        public ViewModel()
+        {
+            this.GDCSource = emp;
+        }
+
+        private ObservableCollection<BusinessObjects> gdcsource;
+
+        public ObservableCollection<BusinessObjects> GDCSource
+        {
+            get { return gdcsource; }
+            set
+            {
+                gdcsource = value;
+                OnPropertyChanged("GDCSource");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+
+    #region GDCSource DataSource
+
+    public class EmployeeDetails : ObservableCollection<BusinessObjects>
+    {
+        private Random rand = new Random();
+
+        public EmployeeDetails()
+        {
+            PopulateCollection();
+        }
+
+        private void PopulateCollection()
+        {
+            this.Clear();
+
+
+            BusinessObjects b = new BusinessObjects()
+            {
+                EmployeeName = "Mart",
+                EmployeeAge = 45,
+                EmployeeArea = "USA",
+                EmployeeSalary = 33000,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Peter",
+                EmployeeAge = 35,
+                EmployeeArea = "UK",
+                EmployeeSalary = 5678,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "James",
+                EmployeeAge = 42,
+                EmployeeArea = "UAE",
+                EmployeeSalary = 18700,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Oliver",
+                EmployeeAge = 36,
+                EmployeeArea = "USA",
+                EmployeeSalary = 67000,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Robert",
+                EmployeeAge = 54,
+                EmployeeArea = "India",
+                EmployeeSalary = 34567,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Suji",
+                EmployeeAge = 45,
+                EmployeeArea = "UK",
+                EmployeeSalary = 90000,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Female"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Mahesh",
+                EmployeeAge = 48,
+                EmployeeArea = "UK",
+                EmployeeSalary = 34567,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Ruby",
+                EmployeeAge = 49,
+                EmployeeArea = "UK",
+                EmployeeSalary = 12345,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Female"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Christain",
+                EmployeeAge = 54,
+                EmployeeArea = "India",
+                EmployeeSalary = 80000,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Aravind",
+                EmployeeAge = 65,
+                EmployeeArea = "India",
+                EmployeeSalary = 12000,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Daniel",
+                EmployeeAge = 56,
+                EmployeeArea = "USA",
+                EmployeeSalary = 16000,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Suhitha Azar",
+                EmployeeAge = 78,
+                EmployeeArea = "UK",
+                EmployeeSalary = 98789,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Female"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Praveen",
+                EmployeeAge = 54,
+                EmployeeArea = "UAE",
+                EmployeeSalary = 45678,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Stephen",
+                EmployeeAge = 45,
+                EmployeeArea = "USA",
+                EmployeeSalary = 21000,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Asha Joseph",
+                EmployeeAge = 56,
+                EmployeeArea = "India",
+                EmployeeSalary = 56787,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Female"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Clarke",
+                EmployeeAge = 67,
+                EmployeeArea = "UK",
+                EmployeeSalary = 1200,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+            this.Add(b);
+            b = new BusinessObjects()
+            {
+                EmployeeName = "Dhileep Venkatesh",
+                EmployeeAge = 45,
+                EmployeeArea = "UK",
+                EmployeeSalary = 45656,
+                ExperienceInMonth = 10,
+                EmployeeGender = "Male"
+            };
+        }
+    }
+    #endregion
+}
